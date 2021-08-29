3afdba74a3a5a04711f2b33fc7538aa68fabfd63
Shogo Yamato <yamash.yamash@gmail.com>
2021-08-29 19:35:12 +0900

    feat(Header): created Header

--- src/App.js
+++ src/App.js
@@ -11,8 +11,16 @@ import React from 'react';
 //   return Star();
 // }
 
+import { GlobalStyle } from './GlobalStyle';
+import Header from './components/Header';
+
 function App() {
-  return <div className="App">Start here.</div>;
+  return (
+    <div className="App">
+      <Header />
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
