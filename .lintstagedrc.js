629f5541a0d4a69d9322d1c7d79ddf21430309fb
Shogo Yamato <yamash.yamash@gmail.com>
2021-09-15 00:36:38 +0900

    chore(deps): 🔗 removed unnecessary dependencies related to sass

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   'src/**/*.{js,jsx,ts,tsx,json}': ['eslint --fix'],
-  'src/**/*.{css,scss,js,jsx,ts,tsx}': ['stylelint --fix'],
-  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': ['prettier --write'],
+  'src/**/*.{css,js,jsx,ts,tsx}': ['stylelint --fix'],
+  'src/**/*.{js,jsx,ts,tsx,json,css,md}': ['prettier --write'],
 };
